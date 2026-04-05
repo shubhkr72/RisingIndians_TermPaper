9bb618bb8bbb0cae0bb24af63441d6478fab15b2
Shubham Kumar <144636150+shubhkr72@users.noreply.github.com>
2026-04-05 10:34:48 +0530

    Change app port to use environment variable

--- backend/app.py
+++ backend/app.py
@@ -49,4 +49,5 @@ def predict_endpoint():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
